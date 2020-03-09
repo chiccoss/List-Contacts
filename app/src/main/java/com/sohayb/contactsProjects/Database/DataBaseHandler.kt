cf511bf0eeaf2e51127e3e6094d25cbdf2962f62
chiccoss <sohaybbahi@gmail.com>
2020-03-09 02:12:41 +0100

    DB insertion

--- app/src/main/java/com/sohayb/contactsProjects/Database/DataBaseHandler.kt
+++ app/src/main/java/com/sohayb/contactsProjects/Database/DataBaseHandler.kt
@@ -5,9 +5,7 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import android.util.Log
 import android.widget.Toast
-import androidx.core.content.contentValuesOf
 import com.sohayb.contactsProjects.Model.Contact
 
 
@@ -53,32 +51,60 @@ class DataBaseHandler(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
     }
 
 
-    fun getAppCategoryDetail(): ArrayList<String?>? {
+    fun getAllData(): ArrayList<Contact?>? {
 
         val selectQuery = "SELECT  * FROM $TABLE_NAME"
         val db = this.readableDatabase
         val cursor: Cursor = db.rawQuery(selectQuery, null)
-        val data: ArrayList<String?>? = null
+        val data: ArrayList<Contact?>? = null
         if (cursor.moveToFirst()) {
             do {
                 // get the data into array, or class variable
-                data!!.add(cursor.getString(1))
-                Toast.makeText(cont, "success in GET IN DB", Toast.LENGTH_SHORT).show()
+                data!!.add(
+                    Contact(
+                        cursor.getString(cursor.getColumnIndex(COL_SURNAME)),
+                        cursor.getString(cursor.getColumnIndex(COL_NAME)),
+                        cursor.getString(cursor.getColumnIndex(COL_PHONE)),
+                        cursor.getString(cursor.getColumnIndex(COL_IMAGE)),
+                        cursor.getString(cursor.getColumnIndex(COL_ADDRESS))
+                    )
+                )
+                Toast.makeText(cont, "GOT ALL DATA", Toast.LENGTH_SHORT).show()
+
 
             } while (cursor.moveToNext())
         }
         cursor.close()
         return data
+
+
     }
 
+    fun getContact(name: String): Contact? {
+        val db = this.writableDatabase
+        val selectQuery = "SELECT  * FROM $TABLE_NAME WHERE $COL_NAME = ?"
+        db.rawQuery(selectQuery, arrayOf(name)).use { // .use requires API 16
+            if (it.moveToFirst()) {
+
+                return Contact(
+                    it.getString(it.getColumnIndex(COL_SURNAME)),
+                    it.getString(it.getColumnIndex(COL_NAME)),
+                    it.getString(it.getColumnIndex(COL_PHONE)),
+                    it.getString(it.getColumnIndex(COL_IMAGE)),
+                    it.getString(it.getColumnIndex(COL_ADDRESS))
+
+                )
+            }
+        }
+        return null
+    }
 
     fun deleteFromDatabase(contact: Contact) {
         val db = writableDatabase
         var phone = contact.phoneNum
-        db.execSQL("delete from $TABLE_NAME where $COL_PHONE==$phone");
-
+        db.execSQL("DELETE FROM $TABLE_NAME WHERE $COL_PHONE LIKE '$phone'");
+        Toast.makeText(cont, "DEETE FROM DB", Toast.LENGTH_SHORT).show()
     }
-
 }
 
 
